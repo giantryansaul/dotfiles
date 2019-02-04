a6a64593a05f8c835292da0dbef4794e662c0635
Ryan Saul <giantryansaul@gmail.com>
2019-02-04 15:03:42 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Homebrew install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+
+# Install JDK 1.8+
+echo "Download the Java 8 JDK"
+open http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+read -p "Press any key to continue once you've installed Java..."
 
 # Workspace setup
 mkdir ~/workspace 
-mkdir ~/personal_workspace
-mkdir ~/notebook && mkdir ~/notebook/journal
-git clone https://github.com/giantryansaul/notebook.git ~/personal_workspace/notebook
+mkdir ~/personal
+brew install 
 brew install httpie jq tmux maven postgresql reattach-to-user-namespace
 
 # Virtualenv
-sudo pip install virtualenvwrapper
+sudo pip3 install virtualenvwrapper
 
 # VIM install
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/vimwiki/vimwiki.git ~/.vim/bundle/vimwiki
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 # Font install
@@ -24,10 +25,10 @@ git clone https://github.com/powerline/fonts ~/.fonts
 ~/.fonts/install.sh
 
 # Tmux setup
-brew install tmux
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # Oh-my-zsh setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -s ~/dotfiles/grs.zsh-theme ~/.oh-my-zsh/themes
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+reattach-to-user-namespace
